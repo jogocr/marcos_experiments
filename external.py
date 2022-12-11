5aaf9f6e40218d234cdf27039f4a39b5a2c9e219
jogocr <johannes_cremer@hotmail.com>
2022-12-11 00:26:39 -0500

    Add my first experiment

--- external.py
+++ external.py
@@ -1,3 +1,3 @@
 import sys
-sys.path.append("/home/vlad/marcos_pack/marcos_client")
-sys.path.append("/home/vlad/marcos_pack/flocra-pulseq")
+sys.path.append("/home/jogo/marcos_pack/marcos_client")
+sys.path.append("/home/jogo/marcos_pack/flocra-pulseq")
